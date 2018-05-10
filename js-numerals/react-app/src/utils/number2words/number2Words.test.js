5e5261ad587c6a2afb744583ae46f75cd79df2c4
Gergely Borgulya <gergely@borgulya.hu>
2018-05-10 09:46:21 +0200

    convert numbers to words

--- js-numerals/react-app/src/utils/number2words/number2Words.test.js
+++ js-numerals/react-app/src/utils/number2words/number2Words.test.js
@@ -4,3 +4,113 @@ test('0 to be zero', () => {
   const { words } = new Number2Words(0)
   expect(words).toBe('zero')
 })
+
+test('1 to be one', () => {
+  const { words } = new Number2Words(1)
+  expect(words).toBe('one')
+})
+
+test('10 to be ten', () => {
+  const { words } = new Number2Words(10)
+  expect(words).toBe('ten')
+})
+
+test('12 to be twelve', () => {
+  const { words } = new Number2Words(12)
+  expect(words).toBe('twelve')
+})
+
+test('20 to be twenty', () => {
+  const { words } = new Number2Words(20)
+  expect(words).toBe('twenty')
+})
+
+test('37 to be thirty-seven', () => {
+  const { words } = new Number2Words(37)
+  expect(words).toBe('thirty-seven')
+})
+
+test('100 to be one hundred', () => {
+  const { words } = new Number2Words(100)
+  expect(words).toBe('one hundred')
+})
+
+test('101 to be one hundred and one', () => {
+  const { words } = new Number2Words(101)
+  expect(words).toBe('one hundred and one')
+})
+
+test('137 to be one hundred and thirty-seven', () => {
+  const { words } = new Number2Words(137)
+  expect(words).toBe('one hundred and thirty-seven')
+})
+
+test('1000 to be one thousand', () => {
+  const { words } = new Number2Words(1000)
+  expect(words).toBe('one thousand')
+})
+
+test('1100 to be eleven hundred', () => {
+  const { words } = new Number2Words(1100)
+  expect(words).toBe('eleven hundred')
+})
+
+test('1201 to be twelve hundred and one', () => {
+  const { words } = new Number2Words(1201)
+  expect(words).toBe('twelve hundred and one')
+})
+
+test('1237 to be twelve hundred and thirty-seven', () => {
+  const { words } = new Number2Words(1237)
+  expect(words).toBe('twelve hundred and thirty-seven')
+})
+
+test('5137 to be fifty-one hundred and thirty-seven', () => {
+  const { words } = new Number2Words(5137)
+  expect(words).toBe('fifty-one hundred and thirty-seven')
+})
+
+test('12437 to be twelve thousand four hundred and thirty-seven', () => {
+  const { words } = new Number2Words(12437)
+  expect(words).toBe('twelve thousand four hundred and thirty-seven')
+})
+
+test('77437 to be seventy-seven thousand four hundred and thirty-seven', () => {
+  const { words } = new Number2Words(77437)
+  expect(words).toBe('seventy-seven thousand four hundred and thirty-seven')
+})
+
+test('500437 to be five hundred thousand four hundred and thirty-seven', () => {
+  const { words } = new Number2Words(500437)
+  expect(words).toBe('five hundred thousand four hundred and thirty-seven')
+})
+
+test('501437 to be five hundred one thousand four hundred and thirty-seven', () => {
+  const { words } = new Number2Words(501437)
+  expect(words).toBe('five hundred one thousand four hundred and thirty-seven')
+})
+
+test('512437 to be five hundred twelve thousand four hundred and thirty-seven', () => {
+  const { words } = new Number2Words(512437)
+  expect(words).toBe('five hundred twelve thousand four hundred and thirty-seven')
+})
+
+test('6512437 to be six million five hundred twelve thousand four hundred and thirty-seven', () => {
+  const { words } = new Number2Words(6512437)
+  expect(words).toBe('six million five hundred twelve thousand four hundred and thirty-seven')
+})
+
+test('11512437 to be eleven million five hundred twelve thousand four hundred and thirty-seven', () => {
+  const { words } = new Number2Words(11512437)
+  expect(words).toBe('eleven million five hundred twelve thousand four hundred and thirty-seven')
+})
+
+test('84512437 to be eighty-four million five hundred twelve thousand four hundred and thirty-seven', () => {
+  const { words } = new Number2Words(84512437)
+  expect(words).toBe('eighty-four million five hundred twelve thousand four hundred and thirty-seven')
+})
+
+test('999999999999999 to be nine hundred ninety-nine trillion nine hundred ninety-nine billion nine hundred ninety-nine million nine hundred ninety-nine thousand nine hundred and ninety-nine', () => {
+  const { words } = new Number2Words(999999999999999)
+  expect(words).toBe('nine hundred ninety-nine trillion nine hundred ninety-nine billion nine hundred ninety-nine million nine hundred ninety-nine thousand nine hundred and ninety-nine')
+})
--- js-numerals/react-app/src/utils/number2words/number2words.js
+++ js-numerals/react-app/src/utils/number2words/number2words.js
@@ -1,4 +1,5 @@
 const ranges = {
+  0: '',
   100: 'hundred',
   1000: 'thousand',
   1000000: 'million',
@@ -6,8 +7,40 @@ const ranges = {
   1000000000000: 'trillion'
 }
 
+const numberNames = {
+  0: 'zero',
+  1: 'one',
+  2: 'two',
+  3: 'three',
+  4: 'four',
+  5: 'five',
+  6: 'six',
+  7: 'seven',
+  8: 'eight',
+  9: 'nine',
+  10: 'ten',
+  11: 'eleven',
+  12: 'twelve',
+  13: 'thirteen',
+  14: 'fourteen',
+  15: 'fifteen',
+  16: 'sixteen',
+  17: 'seventeen',
+  18: 'eighteen',
+  19: 'nineteen',
+  20: 'twenty',
+  30: 'thirty',
+  40: 'fourty',
+  50: 'fifty',
+  60: 'sixty',
+  70: 'seventy',
+  80: 'eighty',
+  90: 'ninety'
+}
+
 export default class Number2Words {
   static checkNumberSanity = number => {
+    // return true if input is a positive integer
     const intNumber = parseInt(number)
     if (
       !isNaN(intNumber) &&
@@ -24,13 +57,115 @@ export default class Number2Words {
   }
 
   convertNumber = () => {
+    // builds words from this.number and stores it in this.words
     const { number } = this
-    console.log(number)
-    if (number === 0) {
-      this.words = `zero`
-    } else {
-      this.words = `something`
+    const limits = Object.keys(ranges).map(r => parseInt(r)).sort((a, b) => b - a)
+    const upperLimit = limits[0] * 1000 - 1
+    if (number > upperLimit) {
+      throw new Error(`Use a number less than or equal to ${upperLimit}`)
     }
+    if (number === 0) {
+      // keep this special case simple
+      this.words = numberNames[number]
+      return
+    } else if (number === 1000) {
+      // otherwise for thousends we use terms like 'twelve-hundred'
+      this.words = `${numberNames[1]} ${ranges[1000]}`
+      return
+    }
+
+    const wordBlocks = []
+    limits.forEach(limit => {
+      if (number >= limit) {
+        if (limit === 0) {
+          const measure = number % 100
+          wordBlocks.push({
+            rangeName: ranges[limit],
+            measure
+          })
+        } else if (limit === 100 && number < 10000) {
+          // special case of double hundreds like 'twelve hundred'
+          const measure = Math.floor(number / 100)
+          wordBlocks.push({
+            rangeName: ranges[limit],
+            measure
+          })
+        } else if (limit === 1000 && number < 10000) {
+          // skip this case as it's covered with double hundreds
+        } else if (limit === 100 && number >= 10000) {
+          // case of single hundreds like 'one hundred'
+          const measure = Math.floor((number % 1000) / limit)
+          wordBlocks.push({
+            rangeName: ranges[limit],
+            measure
+          })
+        } else if (limit !== 100) {
+          const measure = Math.floor((number % (limit * 1000)) / limit)
+          wordBlocks.push({
+            rangeName: ranges[limit],
+            measure
+          })
+        }
+      }
+    })
+    wordBlocks.forEach(block => {
+      const hundreds = Math.floor(block.measure / 100)
+      const tens = Math.floor((block.measure % 100) / 10)
+      const ones = (block.measure % 10)
+      let wordfragment = ''
+      if (hundreds !== 0 || tens !== 0 || ones !== 0) {
+        const wordfragments = {
+          hi: '',
+          low: ''
+        }
+        if (tens === 0 && ones === 0) {
+
+        } else if (tens < 2) {
+          wordfragments.low = numberNames[ones + tens * 10]
+        } else if (ones === 0) {
+          wordfragments.low = numberNames[tens * 10]
+        } else {
+          wordfragments.low = `${numberNames[tens * 10]}-${numberNames[ones]}`
+        }
+        // including the special case of double hundreds like 'twelve hundred'
+        if (hundreds < 21 && hundreds > 0) {
+          wordfragments.hi = `${numberNames[hundreds]} ${ranges[100]}`
+        } else if (hundreds > 20) {
+          const thousands = Math.floor((hundreds % 100) / 10)
+          const hundred = (hundreds % 10)
+          if (thousands < 20) {
+            wordfragments.hi = `${numberNames[hundred + thousands]} ${ranges[100]}`
+          } else if (hundred === 0) {
+            wordfragments.hi = `${numberNames[thousands]} ${ranges[100]}`
+          } else {
+            wordfragments.hi =
+              `${numberNames[thousands]}-${numberNames[hundred]} ${ranges[100]}`
+          }
+        }
+        wordfragment =
+          `${
+            wordfragments.hi
+          }${
+            (wordfragments.hi && wordfragments.low) ? ' ' : ''
+          }${
+            wordfragments.low
+          }`
+      }
+      if (wordfragment !== '') {
+        block.wordfragment =
+          `${wordfragment}${block.rangeName ? ' ': ''}${block.rangeName}`
+      }
+    })
+    // skip blocks without content (like zeros in the middle of the number)
+    const filteredWordBlocks = wordBlocks.filter(block => block.wordfragment)
+    this.words =
+      `${
+        filteredWordBlocks.slice(0, -1).map(b => b.wordfragment).join(' ')
+      }${
+        filteredWordBlocks.length > 1 ? ' and ' : ''
+      }${
+        filteredWordBlocks.slice(-1)[0].wordfragment
+      }`
   }
 
   setNumber = number => {
@@ -58,6 +193,7 @@ if (require.main === module) {
       console.log(number2Words.words)
     } catch(e) {
       console.log(`Error: ${e.message}`)
+      console.error(e)
     }
   }
 }
